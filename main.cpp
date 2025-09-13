cb01a85afe514da8fc07f11960e5cf015f4fb63a
Syeming <3106717682@qq.com>
2025-09-13 22:01:13 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <cmath>
 
-#define eps 1E-12
 
-double function_1(double x){
+double f_origial(double x){
     return 2 * x - log10(x) - 7;
 }
 
-double function(double x){
+double f_iterative(double x){
     return 0.5 * (log10(x) + 7);
 }
 
@@ -16,18 +15,20 @@ double Bisection_method(double a , double b , double epsilon){
     double x_1 = 0;
     double x_2 = 0;
 
+    constexpr double eps = 1e-12;       // 宏定义没有作用域，容易污染全局变量,不利于调试
+
     x_avg = (a + b) / 2;
 
     do {
-        if(fabs(function_1(x_avg)) == 0){
+        if(fabs(f_origial(x_avg)) <= eps){
             return x_avg;
         }
-        else if(function_1(x_avg) * function_1(a) < 0 and function_1(x_avg) * function_1(b) > 0){
+        else if(f_origial(x_avg) * f_origial(a) < 0 and f_origial(x_avg) * f_origial(b) > 0){
             a = a;
             b = x_avg;
 
         }
-        else if(function_1(x_avg)* function_1(b) and function_1(x_avg) * function_1(a) > 0){
+        else if(f_origial(x_avg) * f_origial(b) < 0 and f_origial(x_avg) * f_origial(a) > 0){
             a = x_avg;
             b = b;
         }
@@ -41,11 +42,11 @@ double Bisection_method(double a , double b , double epsilon){
 
 
 
-double Simple_Iteration_Method(double a , double b , double epsilon , double x_0){
+double Simple_Iteration_Method(double epsilon , double x_0){
     double x_b = 0;
     do{
         x_b = x_0;
-        x_0 = function(x_0);
+        x_0 = f_iterative(x_0);
 
     }while(std::fabs(x_b - x_0) >= epsilon);
 
@@ -65,8 +66,8 @@ double Aitken_Method(double x_0, double epsilon){
 
     double x_2_old = 0;
 
-    x_1 = function(x_0);
-    x_2 = function(x_1);
+    x_1 = f_iterative(x_0);
+    x_2 = f_iterative(x_1);
 
     while(std::fabs(x_1 - x_0) >= epsilon){
         x_2_old = x_2;
@@ -75,12 +76,12 @@ double Aitken_Method(double x_0, double epsilon){
         x_1 = x_2_old;
     }
 
-    return x_2;
+    return x_1;
 }
 
 
 double Steffensen_Acceleration_Formula(double x_0){
-    return x_0 - (std::pow(function(x_0) - x_0 , 2)) / (function(function(x_0)) - 2 * function(x_0) + x_0);
+    return x_0 - (std::pow(f_iterative(x_0) - x_0 , 2)) / (f_iterative(f_iterative(x_0)) - 2 * f_iterative(x_0) + x_0);
 }
 
 double Steffensen_Method(double x_0 , double epsilon){
@@ -108,6 +109,10 @@ int main() {
 
     std::cout << "请输入隔根区间：" << std::endl;
     std::cin >> a >> b;
+    if(a >= b or f_origial(a) * f_origial(b) >= 0){
+        std::cerr << "错误！请检查输入的区间是否符合要求。";
+        return 1;
+    }
 
     std::cout << "请输入绝对误差限：" << std::endl;
     std::cin >> epsilon;
@@ -116,7 +121,7 @@ int main() {
     std:: cin >> x_0;
 
     std::cout << "二分法求解结果" << Bisection_method(a , b , epsilon) << std::endl;
-    std::cout << "简单迭代法求解结果：" << Simple_Iteration_Method(a , b , epsilon , x_0) << std::endl;
+    std::cout << "简单迭代法求解结果：" << Simple_Iteration_Method(epsilon , x_0) << std::endl;
     std::cout << "Aitken加速法求解结果：" << Aitken_Method(x_0 , epsilon) << std::endl;
     std::cout << "Steffensen加速法求解结果：" << Steffensen_Method(x_0 , epsilon) << std::endl;
 
